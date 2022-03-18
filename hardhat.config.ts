634f5182f1723e65486b97156b0a84766ddb461b
Igor Dias <igordias.jd@gmail.com>
2022-03-18 15:28:19 -0300

    add generateAccounts command to hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,17 +18,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
+task("generateAccounts", "generate a mmemonic", async (taskArgs, hre) => {
+  const wallet = await hre.ethers.Wallet.createRandom();
+  const mmenonic = wallet._mnemonic();
+  console.log(mmenonic);
+});
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    harmony_testnet: {
+      chainId: 1666700000,
+      accounts: { mnemonic: process.env.mnemonic ? process.env.mnemonic : "" },
+      url: "https://api.s0.b.hmny.io",
     },
   },
   gasReporter: {
